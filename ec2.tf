4f97aaa9aee52d1d42c72b01f72697708cde44f1
Prashant Maheshwari <40426198+prashantmaheshwari@users.noreply.github.com>
2020-12-12 23:27:05 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region     = "eu-west-2"
-  access_key = "AKIAWCPRB4NM6HAPQJVA"
-  secret_key = "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"
-}
-
 resource "aws_instance" "myec2" {
   ami           = "ami-0271d331ac7dab654"
   instance_type = lookup(var.instance_type,terraform.workspace)
